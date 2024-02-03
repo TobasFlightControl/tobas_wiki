59bd5d064088c8610e3d26bc0c2ea8ca50a5d04a
Masa0u0 <masa0u0masa1215@gmail.com>
2024-02-03 15:49:18 +0900

    edit: command_square_trajectory_node.py

--- scripts/command_square_trajecory_node.py
+++ scripts/command_square_trajecory_node.py
@@ -3,70 +3,115 @@
 import rospy
 import actionlib
 
-from tobas_msgs.msg import TakeoffAction, TakeoffGoal, TakeoffResult, PosVelAccYaw
+from tobas_msgs.msg import (
+    Odometry,
+    PosVelAccYaw,
+    TakeoffAction,
+    TakeoffGoal,
+    TakeoffResult,
+    LandAction,
+    LandGoal,
+    LandResult,
+)
 
-ALTITUDE = 3.0  # [m]
+ELEVATION = 3.0  # [m]
 SIDE_LENGTH = 5.0  # [m]
 INTERVAL = 5.0  # [s]
+NUM_CYCLES = 3  # 周回数
 
 
 if __name__ == "__main__":
     # ROSノードの初期化
     rospy.init_node("command_square_trajectory")
 
-    # 離陸アクションクライアントの作成
+    # アクションクライアントの作成
     takeoff_client = actionlib.SimpleActionClient("takeoff_action", TakeoffAction)
+    land_client = actionlib.SimpleActionClient("landing_action", LandAction)
 
     # アクションサーバーが起動するのを待つ
     takeoff_client.wait_for_server()
+    land_client.wait_for_server()
 
-    # アクションゴールを作成
+    # 現在の位置を取得
+    init_odom: Odometry = rospy.wait_for_message("odom", Odometry)
+    x0 = init_odom.pose.pos.x
+    y0 = init_odom.pose.pos.y
+    z0 = init_odom.pose.pos.z
+    rospy.loginfo(f"Start position [m]: ({x0}, {y0}, {z0})")
+
+    # 軌跡を描く高度
+    altitude = z0 + ELEVATION
+
+    # 離陸アクションゴールを作成
     takeoff_goal = TakeoffGoal()
-    takeoff_goal.target_altitude = ALTITUDE
+    takeoff_goal.target_altitude = altitude
     takeoff_goal.target_duration = INTERVAL
 
-    # アクションゴールを送信
+    # 離陸アクションゴールを送信
+    rospy.loginfo("Requesting takeoff.")
     takeoff_client.send_goal_and_wait(takeoff_goal)
 
-    # アクションの結果を取得
+    # 離陸アクションの結果を取得
     takeoff_result: TakeoffResult = takeoff_client.get_result()
     if takeoff_result.error_code < 0:
-        rospy.logerr("Takeoff action failed.")
-        rospy.signal_shutdown()
+        rospy.signal_shutdown("Takeoff action failed.")
 
     # コマンドのパブリッシャーを作成
     command_pub = rospy.Publisher("command/pos_vel_acc_yaw", PosVelAccYaw, queue_size=1)
 
-    # 正方形の頂点を指令し続ける
-    while not rospy.is_shutdown():
+    # 正方形の頂点を指令
+    rospy.loginfo("Start to navigate along the edges of a square.")
+    for _ in range(NUM_CYCLES):
         # 頂点1
         command = PosVelAccYaw()
-        command.pos.x = SIDE_LENGTH / 2
-        command.pos.y = SIDE_LENGTH / 2
-        command.pos.z = ALTITUDE
+        command.pos.x = x0 + SIDE_LENGTH / 2
+        command.pos.y = y0 + SIDE_LENGTH / 2
+        command.pos.z = altitude
         command_pub.publish(command)
         rospy.sleep(INTERVAL)
 
         # 頂点2
         command = PosVelAccYaw()
-        command.pos.x = -SIDE_LENGTH / 2
-        command.pos.y = SIDE_LENGTH / 2
-        command.pos.z = ALTITUDE
+        command.pos.x = x0 - SIDE_LENGTH / 2
+        command.pos.y = y0 + SIDE_LENGTH / 2
+        command.pos.z = altitude
         command_pub.publish(command)
         rospy.sleep(INTERVAL)
 
         # 頂点3
         command = PosVelAccYaw()
-        command.pos.x = -SIDE_LENGTH / 2
-        command.pos.y = -SIDE_LENGTH / 2
-        command.pos.z = ALTITUDE
+        command.pos.x = x0 - SIDE_LENGTH / 2
+        command.pos.y = y0 - SIDE_LENGTH / 2
+        command.pos.z = altitude
         command_pub.publish(command)
         rospy.sleep(INTERVAL)
 
         # 頂点4
         command = PosVelAccYaw()
-        command.pos.x = SIDE_LENGTH / 2
-        command.pos.y = -SIDE_LENGTH / 2
-        command.pos.z = ALTITUDE
+        command.pos.x = x0 + SIDE_LENGTH / 2
+        command.pos.y = y0 - SIDE_LENGTH / 2
+        command.pos.z = altitude
         command_pub.publish(command)
         rospy.sleep(INTERVAL)
+
+    # ホームポジションを指令
+    rospy.loginfo("Returning to the start position.")
+    command = PosVelAccYaw()
+    command.pos.x = x0
+    command.pos.y = y0
+    command.pos.z = altitude
+    command_pub.publish(command)
+    rospy.sleep(INTERVAL)
+
+    # 離陸アクションゴールを作成
+    land_goal = LandGoal()
+
+    # 離陸アクションゴールを送信
+    rospy.loginfo("Requesting landing.")
+    land_client.send_goal_and_wait(land_goal)
+
+    # 離陸アクションの結果を取得
+    land_result: LandResult = land_client.get_result()
+    if land_result.error_code < 0:
+        rospy.logerr("Landing action failed.")
+        rospy.signal_shutdown("Landing action failed")
